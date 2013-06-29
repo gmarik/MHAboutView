02b3acc6cbb83f4a97756f518880f2eed316e7dd
gmarik <gmarik@gmail.com>
2013-06-29 13:57:53 -0500

    MHAboutViewController and dependency on JMStaticContentTable

---
+++ MHAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  MHAboutViewController.h
+//  MHAboutView
+//
+//  Created by Maryan Hratson on 2013-06-29.
+//  Copyright (c) 2013 Maryan Hratson. All rights reserved.
+//
+
+#import <JMStaticContentTable/JMStaticContentTable.h>
+
+@interface MHAboutViewController : JMStaticContentTableViewController
+
+@end
---
+++ MHAboutViewController.m
@@ -0,0 +1,32 @@
+//
+//  MHAboutViewController.m
+//  MHAboutView
+//
+//  Created by Maryan Hratson on 2013-06-29.
+//  Copyright (c) 2013 Maryan Hratson. All rights reserved.
+//
+
+#import "MHAboutViewController.h"
+
+@interface MHAboutViewController ()
+
+@end
+
+@implementation MHAboutViewController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+@end
