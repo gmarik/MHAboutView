04cece2b12b6a85e3eca1deb1a9e9f656d77a030
gmarik <gmarik@gmail.com>
2013-06-29 13:51:32 -0500

    MHAboutView library boilerplate

---
+++ MHAboutView/MHAboutView.h
@@ -0,0 +1,13 @@
+//
+//  MHAboutView.h
+//  MHAboutView
+//
+//  Created by Maryan Hratson on 2013-06-29.
+//  Copyright (c) 2013 Maryan Hratson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MHAboutView : NSObject
+
+@end
---
+++ MHAboutView/MHAboutView.m
@@ -0,0 +1,13 @@
+//
+//  MHAboutView.m
+//  MHAboutView
+//
+//  Created by Maryan Hratson on 2013-06-29.
+//  Copyright (c) 2013 Maryan Hratson. All rights reserved.
+//
+
+#import "MHAboutView.h"
+
+@implementation MHAboutView
+
+@end
